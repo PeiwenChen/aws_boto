a06e7ef3ab6079d0732d6c2d7965f68985953642
Peiwen Chen <page.peiwenchen@gmail.com>
2014-09-22 23:20:52 -0400

    minor changes about variable names

--- boto_aws.py
+++ boto_aws.py
@@ -9,10 +9,15 @@ import boto.ec2
 import time
 from config import *
 
-def launch_instance():
+def launch_instance(ec2_ami, ec2_key, ec2_secret,
+					ec2_group, ec2_key_pair, ec2_tag):
+	
 	"""
-	launch EC2 instance with given AMI
-	read ec2_amis from config
+	launch EC2 instance with given AMI, 
+	AWS_access_key and secret_key, 
+	the security group want to assign for the new generated VM, 
+	the key pair name,
+	and the tag. 
 	"""
 	print "... creating EC2 instance ..."
 	
@@ -21,8 +26,7 @@ def launch_instance():
 	
 	# get security  groups, if the group does not exist, create it
 	try:
-		#group= conn.get_all_security_groups(groupnames=[ec2_group])[0]
-		group= conn.get_all_security_groups()
+		group= conn.get_all_security_groups(groupnames=[ec2_group])[0]
 		print " Security Groups: %s " % group 
 	except conn.ResponseError, e:
 		if e.code == 'InvalidGroup.Notfound':			
@@ -33,21 +37,20 @@ def launch_instance():
 	
 	# get key pair,  if it does not exist, create and save it
 	try:
-		key = conn.get_all_key_pairs(keynames=[ec2_key])[0]
-		print " Key Pair is %s: " % key
+		key = conn.get_all_key_pairs(keynames=[ec2_key_pair])[0]
 	except conn.ResponseError, e:
 		if e.code == 'InvalidKeyPair.NotFound':
 			key_dir = '~/.ssh'
 			print ' Creating a new key pair and save it to %s' % key_dir
-			key = conn.create_key_pair(ec2_key)
+			key = conn.create_key_pair(ec2_key_pair)
 			key.save(key_dir)
 		else:
 			raise
 	
-	reservation = conn.run_instances(ec2_amis,
-					key_name=ec2_key,
+	reservation = conn.run_instances(ec2_ami,
+					key_name=ec2_key_pair,
 					instance_type=ec2_instancetype, 
-					security_groups=[ec2_group])
+					security_groups=[group])
 			
 	instance = reservation.instances[0]
 	conn.create_tags([instance.id], {"Name":ec2_tag})
@@ -77,11 +80,13 @@ def terminate_instance(instance_id):
 	"""
 	print "... terminating EC2 instance ..."
 	conn = boto.connect_ec2(ec2_key,ec2_secret)
-	conn = terminate_instances(instance_ids=[instance_id])
+	conn.terminate_instances(instance_ids=[instance_id])
 
 
 if __name__ == "__main__":
-	instance = launch_instance()
+	
+	instance = launch_instance(ec2_ami, ec2_key, ec2_secret, 
+							ec2_group, ec2_key_pair, ec2_tag)
 	print " Sleep 200s before stopping the instance ... "
 	time.sleep(200)
 	stop_instance(instance.id)
